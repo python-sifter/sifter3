0735575939669cfd2eb20d14d20bef10c9e6ab56
Jeff Mahoney <jeffm@suse.com>
2019-01-04 10:09:43 -0500

    sifter: add python3 support to ascii_casemap comparators
    string.maketrans was removed in Python 3.  str.maketrans is the
    appropriate function now, but it doesn't exist in Python 2.  This
    adds a class member that uses the right one.

--- sifter/comparators/ascii_casemap.py
+++ sifter/comparators/ascii_casemap.py
@@ -7,10 +7,16 @@ __all__ = ('ComparatorASCIICasemap',)
 class ComparatorASCIICasemap(ComparatorOctet):
 
     COMPARATOR_ID = 'i;ascii-casemap'
+    maketrans = None
 
     @classmethod
     def sort_key(cls, s):
-        return s.translate(string.maketrans(string.ascii_lowercase,
+        if cls.maketrans is None:
+            try:
+                cls.maketrans = str.maketrans
+            except AttributeError:
+                cls.maketrans = string.maketrans
+        return s.translate(cls.maketrans(string.ascii_lowercase,
             string.ascii_uppercase))
 
 ComparatorASCIICasemap.register()
@@ -18,10 +24,16 @@ ComparatorASCIICasemap.register()
 class ComparatorASCIICasemapnoi(ComparatorOctet):
 
     COMPARATOR_ID = ';ascii-casemap'
+    maketrans = None
 
     @classmethod
     def sort_key(cls, s):
-        return s.translate(string.maketrans(string.ascii_lowercase,
+        if cls.maketrans is None:
+            try:
+                cls.maketrans = str.maketrans
+            except AttributeError:
+                cls.maketrans = string.maketrans
+        return s.translate(cls.maketrans(string.ascii_lowercase,
             string.ascii_uppercase))
 
 ComparatorASCIICasemapnoi.register()
