b634471e8c556e19eef753f806be8589a408b14b
Manfred Kaiser <manfred.kaiser@logfile.at>
2020-10-09 13:30:00 +0200

    added ihave to supported extensions

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="sifter3",
-    version="0.2.4",
+    version="0.2.5",
     author="Manfred Kaiser, Gary Peck",
     author_email="manfred.kaiser@logfile.at, gary@realify.com",
     url="https://sifter3.readthedocs.io/en/latest/",
@@ -78,6 +78,7 @@ setup(
             'true = sifter.tests.true:TestTrue',
             'valid_notify_method = sifter.tests.notify:TestValidNotifyMethod',
             'notify_method_capability = sifter.tests.notify:TestValidNotifyMethod',
+            'ihave = sifter.tests.ihave:TestIHave',
             # sifter comparators
             'ascii_casemap = sifter.comparators.ascii_casemap:ComparatorASCIICasemap',
             'ascii_casemap_noi = sifter.comparators.ascii_casemap:ComparatorASCIICasemapnoi',
--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -28,6 +28,7 @@ def test_evaulation():
         ("evaluation_1.msg", "evaluation_5.rules", [('reject', 'I do not accept messages from\nthis address.\n.\n')]),
         ("evaluation_1.msg", "evaluation_6.rules", [('reject', 'I do not accept messages from this address.')]),
         ("evaluation_1.msg", "evaluation_7.rules", [('reject', 'I do not accept messages from/* this is\nnot a comment */this address.\n.\n')]),
+        ("evaluation_1.msg", "evaluation_ihave_reject.rules", [('reject', 'I do not accept messages from this address.')]),
     )
 
     for messagefile, rulefile, evaluated_rules in EVAL_RESULTS:
